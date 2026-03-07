236912843d07242be9456738562c97412335658f
Arun Mishra <136677308+arun-mishra20@users.noreply.github.com>
2026-03-07 13:14:43 +0530

    add fixes

--- apps/api/src/modules/auth/presentation/dtos/webauthn.dto.ts
+++ apps/api/src/modules/auth/presentation/dtos/webauthn.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger'
+import { Type } from 'class-transformer'
 import {
   IsEmail,
   IsNotEmptyObject,
@@ -6,7 +7,6 @@ import {
   IsString,
   ValidateNested,
 } from 'class-validator'
-import { Type } from 'class-transformer'
 
 import type {
   AuthenticationResponseJSON,
